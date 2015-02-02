31bd7c84d9eb4d9756998e67982a00150c76db7b
Ricardo Oliveira <ricardolotr@gmail.com>
2015-02-02 16:01:11 +0000

    added getExportCsvFeedUrl

--- src/Google/Spreadsheet/Worksheet.php
+++ src/Google/Spreadsheet/Worksheet.php
@@ -180,4 +180,14 @@ class Worksheet
     {
         return Util::getLinkHref($this->xml, 'http://schemas.google.com/spreadsheets/2006#cellsfeed');
     }
+    
+    /**
+     * Get the export csv feed url
+     *
+     * @return string
+     */
+    public function getExportCsvFeedUrl()
+    {
+        return Util::getLinkHref($this->xml, 'http://schemas.google.com/spreadsheets/2006#exportcsv');
+    }
 }
